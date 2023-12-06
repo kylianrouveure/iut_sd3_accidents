b875a6251e7ba78e9a4fb80b247e703fcf0c7631
kyxial <56232624+kylianrouveure@users.noreply.github.com>
2023-12-06 16:42:12 +0100

    code

--- IUT_SD3_Github/step2.py
+++ IUT_SD3_Github/step2.py
@@ -1,3 +1,58 @@
-import pandas as pd
-
 print("Bon courage pour la suite")
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+carac = pd.read_csv("carac.csv",sep=';')
+lieux = pd.read_csv("lieux.csv",sep=';')
+veh = pd.read_csv("veh.csv",sep=';')
+vict = pd.read_csv("vict.csv",sep=';')
+
+victime = vict.merge(veh,on=['Num_Acc','num_veh'])
+accident = carac.merge(lieux,on = 'Num_Acc')
+
+df = victime.merge(accident,on='Num_Acc')
+
+nan_values = df.isna().sum()
+
+nan_values = nan_values.sort_values(ascending=True)*100/127951
+
+ax = nan_values.plot(kind='barh', 
+                     figsize=(8, 10), 
+                     color='#AF7AC5',
+                     zorder=2,
+                     width=0.85)
+
+ax.spines['top'].set_visible(False)
+ax.spines['left'].set_visible(False)
+ax.spines['right'].set_visible(False)
+ax.spines['bottom'].set_visible(False)
+
+ax.tick_params(axis="both", 
+               which="both", 
+               bottom="off", 
+               top="off", 
+               labelbottom="on", 
+               left="off", 
+               right="off", 
+               labelleft="on")
+
+vals = ax.get_xticks()
+
+for tick in vals:
+  ax.axvline(x=tick, linestyle='dashed', alpha=0.4, color='#eeeeee', zorder=1)
+
+plt.savefig("step2/graph.png")
+
+nans = ['v1','v2','lartpc',
+       'larrout','locp','etatp',
+       'actp','voie','pr1',
+       'pr','place']
+df.corr()
+df.var()
+
+df = df.drop(columns = nans)
+df = df.dropna()
+
+df.to_csv("step2/missing_values_deleted.csv")
